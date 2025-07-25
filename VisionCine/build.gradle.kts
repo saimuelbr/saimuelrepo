333a166353b2006e21a3760b18da03046d775119
saimuelbr <pixelzdroid777@gmail.com>
2025-07-25 20:47:28 -0300

    ah shit, here we go again. VisionCine Added again

---
+++ VisionCine/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Baseado em MultiAPI. Filmes, Séries, Animes e Doramas."
+    language = "pt-br"
+    authors = listOf("saimuelbr")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.visioncine-1.com.br/temas/pixer/assets/img/icon_free.png"
+    isCrossPlatform = true
+} 
