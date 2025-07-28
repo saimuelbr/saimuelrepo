6543f95257e21a401f5059caa73e6b367244dcd1
saimuelbr <pixelzdroid777@gmail.com>
2025-07-27 23:15:29 -0300

    Adding EmbedCanais

---
+++ EmbedCanais/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Canais de TV ao vivo do EmbedCanais. Esportes, Desenhos, Noticias e muito mais."
+    language = "pt-br"
+    authors = listOf("saimuelbr")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://embedcanaistv.com/wp-content/uploads/2024/11/cropped-cropped-iconn-192x192.png"
+    isCrossPlatform = true
+} 
