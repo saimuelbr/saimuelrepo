0b558c8809f6c776d0c60dba3694ad1f18409c8a
saimuelbr <pixelzdroid777@gmail.com>
2025-08-15 19:06:04 -0300

    Adding UltraCine

---
+++ UltraCine/src/main/kotlin/com/UltraCine/EmbedPlayExtractor.kt
@@ -0,0 +1,13 @@
+package com.UltraCine
+
+import com.lagradost.cloudstream3.extractors.VidStack
+
+class EmbedPlayUpnsPro : VidStack() {
+    override var name = "EmbedPlay UpnsPro"
+    override var mainUrl = "https://embedplay.upns.pro"
+}
+
+class EmbedPlayUpnOne : VidStack() {
+    override var name = "EmbedPlay UpnOne"
+    override var mainUrl = "https://embedplay.upn.one"
+}
